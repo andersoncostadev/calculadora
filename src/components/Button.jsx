7500058e27744de818ff6d4d701f0e1e1d0094f7
andersoncostadev <78692782+andersoncostadev@users.noreply.github.com>
2021-09-22 09:35:55 -0300

    calculadora commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import './Button.css';
+
+export default props => {
+
+
+  return (
+    <button
+      onClick={event => props.click && props.click(props.label)}
+      className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+      `}>
+      {props.label}
+    </button>
+  )
+}
